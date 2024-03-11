6e5b8ca06799c4662f11e0f45e67481f3a9e74bd
Ipek Canli <119710326+i99c@users.noreply.github.com>
2024-03-11 16:03:02 +0300

    yeni html sayfaları, url tanımlamaları

--- urunler/urls.py
+++ urunler/urls.py
@@ -3,5 +3,16 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('abiye/', abiye, name='abiye'),
+    path('kadin-ayakkabi/', kadinAyakkabi, name='kayakkabi'),
+    path('kozmetik/', kozmetik, name='kozmetik'),
+    path('tamamlayici/', tamamlayici, name='ktmmurun'),
+    path('taki/', taki, name='taki'),
+    path('gomlek/', gomlek, name='gomlek'),
+    path('takim/', takim, name='takim'),
+    path('ceket/', ceket, name='ceket'),
+    path('yelek/', yelek, name='yelek'),
+    path('erkek-ayakkabi/', eayakkabi, name='eayakkabi'),
+    path('erkek-tamamlayici/', etmmurun, name='etmmurun'),
    
 ]
--- urunler/views.py
+++ urunler/views.py
@@ -5,3 +5,35 @@ from django.shortcuts import render
 def index(request):
     return render(request,"index.html")
 
+def abiye(request):
+    return render(request,"abiye.html")
+
+def kadinAyakkabi(request):
+    return render(request, "kayakkabi.html")
+
+def kozmetik(request):
+    return render(request, "kozmetik.html")
+
+def tamamlayici(request):
+    return render(request, "ktmmurun.html")
+
+def taki(request):
+    return render(request, "taki.html")
+
+def gomlek(request):
+    return render(request, "gomlek.html")
+
+def takim(request):
+    return render(request, "takim.html")
+
+def ceket(request):
+    return render(request, "ceket.html")
+
+def yelek(request):
+    return render(request, "yelek.html")
+
+def eayakkabi(request):
+    return render(request, "eayakkabi.html")
+
+def etmmurun(request):
+    return render(request, "etmmurun.html")
