dd44378439e74ec85b6ebab5c0a5ab1d32e74908
Ipek Canli <119710326+i99c@users.noreply.github.com>
2024-03-15 13:36:14 +0300

    güncellemeler

--- static/js/script.js
+++ static/js/script.js
@@ -1,30 +1,40 @@
-// script.js
+
+function addToCart(name, price, image_url, size) {
+    let productHTML = `
+    <div class="col-md-4">
+      <div class="sepet-urun">
+        <img class="img" src="${image_url}" alt="Ürün Görseli" />
+        <div class="content">
+          <div class="product-info">
+            <h2 class="product-name">${name}</h2>
+            <h4 class="price">${price}</h4>
+          </div>
+          <hr />
+          <div class="actions">
+            <button class="remove-btn"><i class="fa-solid fa-trash" style="color: #63E6BE;"></i></button>
+            <input type="number" class="quantity-input" value="1" />
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+    console.log("Sepete eklenen ürün:", name, price, image_url, size);
+}
+
+// Beden butonlarına tıklandığında ürünü sepete eklemek için event listener ekleyelim
 document.addEventListener('DOMContentLoaded', function () {
-    let favButtons = document.querySelectorAll('.fav-btn');
-    let cartButtons = document.querySelectorAll('.cart-btn');
-    let deleteButtons = document.querySelectorAll('.delete-btn');
+    let sizeButtons = document.querySelectorAll('.size-btn');
 
-    // Favori ekleme işlevi
-    favButtons.forEach(function (button) {
-        button.addEventListener('click', function () {
-            console.log('Favorilere eklendi: ' + this.parentElement.parentElement.querySelector('.product-name').innerText);
-            // Favorilere ekleme işlemleri buraya gelecek
-        });
-    });
+    sizeButtons.forEach(function (button) {
+        button.addEventListener('click', function (event) {
+            let productElement = event.target.closest('.yeni-yapi');
+            let name = productElement.querySelector('.product-name').textContent;
+            let price = productElement.querySelector('.price').textContent;
+            let image_url = productElement.querySelector('.img').getAttribute('src');
+            let size = event.target.textContent;
 
-    // Sepete ekleme işlevi
-    cartButtons.forEach(function (button) {
-        button.addEventListener('click', function () {
-            console.log('Sepete eklendi: ' + this.parentElement.parentElement.querySelector('.product-name').innerText);
-            // Sepete ekleme işlemleri buraya gelecek
-        });
-    });
-
-    // Ürünü silme işlevi
-    deleteButtons.forEach(function (button) {
-        button.addEventListener('click', function () {
-            console.log('Ürün silindi: ' + this.parentElement.parentElement.querySelector('.product-name').innerText);
-            // Ürünü silme işlemleri buraya gelecek
+            addToCart(name, price, image_url, size);
         });
     });
 });
+
